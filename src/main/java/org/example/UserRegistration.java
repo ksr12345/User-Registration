bd1acd4a02b433e28c3fe2239dce6cb0f31e0801
ksr12345 <kaustubh.raut@somaiya.edu>
2024-08-08 12:47:44 +0530

    Added:  Email Validation

--- src/main/java/org/example/UserRegistration.java
+++ src/main/java/org/example/UserRegistration.java
@@ -8,16 +8,33 @@ public class UserRegistration {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter first name:");
-        String fName=sc.next();
+        String fName = sc.next();
         String pattern = "^[A-Z][a-zA-Z]+$";
         Pattern compiledPattern = Pattern.compile(pattern);
         Matcher matcher = compiledPattern.matcher(fName);
-        System.out.println(matcher.matches());
+        if (matcher.matches()) {
+            System.out.println("VALID");
+        }else {
+            System.out.println("INVALID");
+        }
 
         System.out.println("Enter last name:");
-        String lName=sc.next();
+        String lName = sc.next();
         Matcher matcher2 = compiledPattern.matcher(lName);
-        System.out.println(matcher2.matches());
-
+        if (matcher2.matches()) {
+            System.out.println("VALID");
+        } else{
+            System.out.println("INVALID");
+        }
+        System.out.println("Enter email-id: ");
+        String email=sc.next();
+        String pattern2 = "^[a-zA-Z0-9.%+-]+\\.[a-zA-Z0-9.%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}(\\.[a-zA-Z]{2,6})?$";
+        Pattern compiledPattern2 = Pattern.compile(pattern2);
+        Matcher matcher3 = compiledPattern2.matcher(email);
+        if(matcher3.matches()) {
+            System.out.println("VALID");
+        }else {
+            System.out.println("INVALID");
+        }
     }
 }
